b7a3c501378265300999e1f4e3f4d30404e38380
claud-car <claudiocarchietti@yahoo.it>
2021-06-11 12:08:52 +0200

    vue cli

---
+++ src/views/Page.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page">
+    <h1>pppppppppppppppppppp</h1>
+    <Header msg="Questo è l'header di page" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from "@/components/Header.vue";
+
+
+export default {
+  name: "Page",
+  components: {
+    Header,
+  },
+};
+</script>
+
